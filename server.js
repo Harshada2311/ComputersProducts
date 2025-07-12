a20fa05b98fd3ad813a74e7fbe9fa809d66968ed
Harshada Shinde <harshdashinde6@gmail.com>
2025-07-12 16:36:42 +0530

    Initial backend commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const authRoutes = require('./routes/auth');
+const productRoutes = require('./routes/product');
+const wishlistRoutes = require('./routes/wishlist');
+const authMiddleware = require('./middleware/auth');
+
+dotenv.config();
+const app = express();
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+
+  .then(() => console.log('MongoDB Connected'))
+  .catch((err) => console.error(err));
+
+app.use('/auth', authRoutes);
+app.use('/products', authMiddleware, productRoutes);
+app.use('/wishlist', authMiddleware, wishlistRoutes);
+
+const PORT = process.env.PORT || 5009;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
